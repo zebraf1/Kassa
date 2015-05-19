16d138ac0f3723530ed4013a43a7e6d4e6f9ff0a
Jaak Tamre <jaak.tamre@gmail.com>
2015-05-19 09:50:31 +0300

    #1 Save current product prices to report rows when inserting a new report

--- src/Rotalia/InventoryBundle/Model/ReportRow.php
+++ src/Rotalia/InventoryBundle/Model/ReportRow.php
@@ -2,8 +2,22 @@
 
 namespace Rotalia\InventoryBundle\Model;
 
+use PropelPDO;
 use Rotalia\InventoryBundle\Model\om\BaseReportRow;
 
 class ReportRow extends BaseReportRow
 {
+    /**
+     * @inheritdoc
+     */
+    public function preInsert(PropelPDO $con = null)
+    {
+        //Use related Product price and set it as current price upon insertion
+        if ($this->getCurrentPrice() === null) {
+            $product = $this->getProduct($con);
+            $this->setCurrentPrice($product->getPrice());
+        }
+
+        return true;
+    }
 }
