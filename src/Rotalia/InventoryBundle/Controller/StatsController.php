22f7f09d36f63a2454fe7e8b6b3f696af8659e5f
Jaak Tamre <jaak.tamre@gmail.com>
2015-09-20 15:55:49 +0300

    #20 Created product statistics page

---
+++ src/Rotalia/InventoryBundle/Controller/StatsController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Rotalia\InventoryBundle\Controller;
+
+
+use Rotalia\InventoryBundle\Classes\XClassifier;
+use Rotalia\InventoryBundle\Model\Report;
+use Rotalia\InventoryBundle\Model\ReportQuery;
+use \DateTime;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+class StatsController extends DefaultController
+{
+    /**
+     * Display Product statistics
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function productsAction()
+    {
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            throw new AccessDeniedException();
+        }
+
+        //Find all reports within last 30 days
+        $periodStart = new DateTime('-30 days');
+        $periodEnd = new DateTime('now');
+
+        /** @var Report[] $reports */
+        $reports = ReportQuery::create()
+            ->filterByCreatedAt($periodStart->format('c'), \Criteria::GREATER_EQUAL)
+            ->filterByCreatedAt($periodEnd->format('c'), \Criteria::LESS_EQUAL)
+            ->orderByCreatedAt()
+            ->joinReportRow('report_row')
+            ->useReportRowQuery('product', \Criteria::INNER_JOIN)
+                ->useProductQuery(null, \Criteria::INNER_JOIN)
+                    ->filterByStatusId(XClassifier::STATUS_ACTIVE)
+                ->endUse()
+            ->endUse()
+            ->with('report_row')
+            ->with('product')
+            ->find()
+        ;
+
+        $tempTable = [];
+
+        foreach ($reports as $report) {
+            foreach ($report->getReportRows() as $reportRow) {
+                $productId = $reportRow->getProductId();
+
+                if ($report->isUpdate() && !isset($tempTable[$productId])) {
+                    continue; //Skip all updates before the initial value is set for a product
+                }
+
+                if (!isset($tempTable[$productId])) {
+                    $tempTable[$productId] = [
+                        'initAmount' => $reportRow->getAmount(),
+                        'addedAmount' => 0,
+                        'finalAmount' => 0,
+                        'productName' => $reportRow->getProduct()->getName()
+                    ];
+                }
+
+                if ($report->isUpdate()) {
+                    $tempTable[$productId]['addedAmount'] += $reportRow->getAmount();
+                } else {
+                    $tempTable[$productId]['finalAmount'] = $reportRow->getAmount();
+                }
+            }
+        }
+
+        $resultTable = [];
+
+        setlocale(LC_TIME, 'et_EE');
+
+        foreach ($tempTable as $productId => $amounts) {
+            $consumed = $amounts['initAmount'] + $amounts['addedAmount'] - $amounts['finalAmount'];
+            $avgConsumption = $consumed > 0 ? round($consumed / 30, 1) : 0;
+            if ($amounts['finalAmount'] > 0 && $avgConsumption > 0) {
+                $supplyForDays = round($amounts['finalAmount'] / $avgConsumption);
+                $availableUntil = strftime('%e. %B %Y', strtotime('+'.$supplyForDays.' day'));
+                $sort = 2;
+            } else {
+                $supplyForDays = null;
+                if ($amounts['finalAmount'] <= 0 && $avgConsumption > 0) {
+                    $availableUntil = 'Toode on otsas, telli juurde';
+                    $sort = 1;
+                } elseif ($amounts['finalAmount'] <= 0 && $avgConsumption <= 0) {
+                    $availableUntil = 'Toode on otsas ja seda ei kulu';
+                    $sort = 4;
+                } elseif ($amounts['finalAmount'] > 0 && $avgConsumption <= 0) {
+                    $availableUntil = 'Toodet ei kulu';
+                    $sort = 3;
+                } else {
+                    $availableUntil = '?';
+                    $sort = 5;
+                }
+            }
+
+            $monthlySupply = round($avgConsumption * 30);
+
+            $resultTable[] = [
+                'productName' => $amounts['productName'],
+                'avgConsumption' => $avgConsumption,
+                'availableUntil' => $availableUntil,
+                'monthlySupply' => $monthlySupply,
+                'currentSupply' => $amounts['finalAmount'],
+                'supplyForDays' => $supplyForDays,
+                'sort' => $sort,
+            ];
+        }
+
+        //Sort results
+        for ($i = 0; $i < count($resultTable); $i++) {
+            for ($j = $i + 1; $j < count($resultTable); $j++) {
+                if ($resultTable[$i]['sort'] < $resultTable[$j]['sort']){
+                    continue; //Already in order
+                }
+
+                //When sort is equal and 2, then compare available until
+                if ($resultTable[$i]['sort'] > $resultTable[$j]['sort'] || $resultTable[$i]['sort'] == 2 && $resultTable[$i]['supplyForDays'] > $resultTable[$j]['supplyForDays']) {
+                    $tempSort = $resultTable[$i];
+                    $resultTable[$i] = $resultTable[$j];
+                    $resultTable[$j] = $tempSort;
+                }
+            }
+        }
+
+        return $this->render('RotaliaInventoryBundle:Stats:products.html.twig', [
+            'resultTable' => $resultTable
+        ]);
+    }
+}
