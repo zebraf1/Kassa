197d343748fe8a1071ddb7d208af3e6f0a67600a
Jaak Tamre <jaak.tamre@gmail.com>
2015-10-03 19:12:47 +0300

    #22 Created report edit page #23 Created report view page, added print layout style

--- src/Rotalia/InventoryBundle/Controller/ReportController.php
+++ src/Rotalia/InventoryBundle/Controller/ReportController.php
@@ -24,25 +24,15 @@ class ReportController extends DefaultController
      */
     public function listAction(Request $request)
     {
-        $isUpdate = $request->get('update', false);
-        if ($isUpdate && !$this->isGranted('ROLE_ADMIN')) {
-            throw new AccessDeniedException();
-        }
+        $activeReport = ReportQuery::create()
+            ->filterByCreatedToday()
+            ->filterByType(Report::TYPE_VERIFICATION) //Avoid update reports as active
+            ->findOneOrCreate()
+        ;
 
-        if ($isUpdate) {
-            $activeReport = new Report();
+        if ($activeReport->isNew()) {
+            //Fix: Save time, not just date
             $activeReport->setCreatedAt(new DateTime());
-            $activeReport->setType(Report::TYPE_UPDATE);
-        } else {
-            $activeReport = ReportQuery::create()
-                ->filterByCreatedToday()
-                ->filterByType(Report::TYPE_VERIFICATION) //Avoid update reports as active
-                ->findOneOrCreate()
-            ;
-            if ($activeReport->isNew()) {
-                //Fix: Save time, not just date
-                $activeReport->setCreatedAt(new DateTime());
-            }
         }
 
         $reportForm = $this->createForm(new ReportType(), $activeReport);
@@ -59,44 +49,34 @@ class ReportController extends DefaultController
             $this->setFlash('error', 'Aruande sisestamisel tekkis vigu!');
         }
 
-        if (!$isUpdate) {
-            /** @var ReportQuery $reportQuery */
-            $reportQuery = ReportQuery::create()
-                ->orderByCreatedAt(\Criteria::DESC)
-                ->limit(4);
+        /** @var ReportQuery $reportQuery */
+        $reportQuery = ReportQuery::create()
+            ->orderByCreatedAt(\Criteria::DESC)
+            ->limit(4);
 
-            //Exclude active report, which is passed separately
-            if (!$activeReport->isNew()) {
-                $reportQuery->filterById($activeReport->getId(), \Criteria::NOT_EQUAL);
-            }
-
-            /** @var Report[]|\PropelObjectCollection $reports */
-            $reports = $reportQuery->find();
-
-            //Sort by created at asc
-            $reports->uasort(function($a, $b) {
-                /** @var $a Report */
-                /** @var $b Report */
-                return $a->getCreatedAt() > $b->getCreatedAt();
-            });
-
-            $products = ProductQuery::getActiveProductsFirst();
-
-            return $this->render('RotaliaInventoryBundle:Report:list.html.twig', [
-                'products' => $products,
-                'reports' => $reports,
-                'activeReport' => $activeReport,
-                'reportForm' => $reportForm->createView(),
-            ]);
-        } else {
-            $products = ProductQuery::getActiveProducts();
-
-            return $this->render('RotaliaInventoryBundle:Report:update.html.twig', [
-                'products' => $products,
-                'activeReport' => $activeReport,
-                'reportForm' => $reportForm->createView(),
-            ]);
+        //Exclude active report, which is passed separately
+        if (!$activeReport->isNew()) {
+            $reportQuery->filterById($activeReport->getId(), \Criteria::NOT_EQUAL);
         }
+
+        /** @var Report[]|\PropelObjectCollection $reports */
+        $reports = $reportQuery->find();
+
+        //Sort by created at asc
+        $reports->uasort(function($a, $b) {
+            /** @var $a Report */
+            /** @var $b Report */
+            return $a->getCreatedAt() > $b->getCreatedAt();
+        });
+
+        $products = ProductQuery::getActiveProductsFirst();
+
+        return $this->render('RotaliaInventoryBundle:Report:list.html.twig', [
+            'products' => $products,
+            'reports' => $reports,
+            'activeReport' => $activeReport,
+            'reportForm' => $reportForm->createView(),
+        ]);
     }
 
     /**
@@ -125,6 +105,103 @@ class ReportController extends DefaultController
         return $this->render('RotaliaInventoryBundle:Report:browse.html.twig', [
             'products' => $products,
             'reports' => $reports,
+        ]);
+    }
+
+    /**
+     * View a single report
+     *
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \PropelException
+     */
+    public function viewAction($id)
+    {
+        /** @var Report $report */
+        $report = ReportQuery::create()
+            ->useReportRowQuery('report_row')
+                ->useProductQuery('product')
+                    ->orderBySeq()
+                ->endUse()
+            ->endUse()
+            ->with('report_row')
+            ->with('product')
+            ->findPk($id)
+        ;
+
+        $products = [];
+
+        foreach ($report->getReportRows() as $row) {
+            $products[] = $row->getProduct();
+        }
+
+        return $this->render('RotaliaInventoryBundle:Report:report.html.twig', [
+            'products' => $products,
+            'report' => $report,
+        ]);
+    }
+
+    /**
+     * Update a single report
+     *
+     * @param Request $request
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \PropelException
+     */
+    public function updateAction(Request $request, $id = null)
+    {
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            throw new AccessDeniedException();
+        }
+
+        if ($id === null) {
+            $report = new Report();
+            $report->setCreatedAt(new DateTime());
+            $report->setType(Report::TYPE_UPDATE);
+
+            $products = ProductQuery::getActiveProducts();
+        } else {
+            /** @var Report $report */
+            $report = ReportQuery::create()
+                ->useReportRowQuery('report_row')
+                ->useProductQuery('product')
+                ->orderBySeq()
+                ->endUse()
+                ->endUse()
+                ->with('report_row')
+                ->with('product')
+                ->findPk($id)
+            ;
+
+            $products = [];
+
+            foreach ($report->getReportRows() as $row) {
+                $products[] = $row->getProduct();
+            }
+        }
+
+        $reportForm = $this->createForm(new ReportType(true), $report);
+
+        $reportForm->handleRequest($request);
+
+        if ($reportForm->isValid()) {
+            if ($report->isNew()) {
+                $report->setMember($this->getUser()->getMember());
+                $report->updateRowPrices();
+            }
+            $report->save();
+            $this->setFlash('ok', 'Aruanne salvestatud.');
+
+            return $this->redirect($this->generateUrl('RotaliaReport_list'));
+        } elseif ($reportForm->isSubmitted()) {
+            $this->setFlash('error', 'Aruande sisestamisel tekkis vigu!');
+        }
+
+        return $this->render('RotaliaInventoryBundle:Report:update.html.twig', [
+            'products' => $products,
+            'report' => $report,
+            'reportForm' => $reportForm->createView(),
         ]);
     }
 }
