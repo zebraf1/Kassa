efde8795de3d9c2698627919c31bde3fd66ec9c6
Jaak Tamre <jaak.tamre@gmail.com>
2017-07-27 23:34:06 +0300

    #72 Added API doc for members request. Added conventId filter.

--- src/Rotalia/APIBundle/Controller/MembersController.php
+++ src/Rotalia/APIBundle/Controller/MembersController.php
@@ -7,13 +7,33 @@ use Rotalia\InventoryBundle\Component\HttpFoundation\JSendResponse;
 use Rotalia\UserBundle\Model\Member;
 use Rotalia\UserBundle\Model\MemberQuery;
 use Symfony\Component\HttpFoundation\Request;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
 class MembersController extends DefaultController
 {
+    /**
+     * @param Request $request
+     * @return JSendResponse
+     * @ApiDoc(
+     *     resource = true,
+     *     statusCodes = {
+     *          200 = "Returned when successful",
+     *     },
+     *     description="Fetch Members list",
+     *     section="Members",
+     *     filters={
+     *          {"name"="name","type"="string"},
+     *          {"name"="conventId","type"="int"},
+     *          {"name"="offset","type"="int","default"="0"},
+     *          {"name"="limit","type"="int","default"="10"},
+     *     }
+     * )
+     */
     public function listAction(Request $request)
     {
         $memberQuery = MemberQuery::create();
         $name = $request->get('name');
+        $conventId = $request->get('conventId');
         $limit = $request->get('limit', 10);
         $offset = $request->get('offset', 0);
 
@@ -22,6 +42,10 @@ class MembersController extends DefaultController
                 ->limit($limit)
                 ->offset($offset)
             ;
+        }
+
+        if (!empty($conventId)) {
+            $memberQuery->filterByKoondisedId($conventId);
         }
 
         if (!empty($name)) {
