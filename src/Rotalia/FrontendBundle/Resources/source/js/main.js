39fda7d57fc23c072c9313fadec3f09cdd045301
Tanel K <tanel.kiis@gmail.com>
2017-08-08 08:46:42 +0300

    Upgrade to polymer 2.0

--- src/Rotalia/FrontendBundle/Resources/source/js/main.js
+++ src/Rotalia/FrontendBundle/Resources/source/js/main.js
@@ -1,23 +1,8 @@
 (function () {
-    // setup Polymer options
-    window.Polymer = {lazyRegister: true, dom: 'shadow'};
-
     //hide the skeleton
     window.addEventListener('kassa-app-loaded', function () {
         document.body.className = '';
     });
-
-    // load webcomponents polyfills
-    var webComponentsSuported = 'registerElement' in document
-        && 'import' in document.createElement('link')
-        && 'content' in document.createElement('template');
-
-    if (!webComponentsSuported) {
-        var script = document.createElement('script');
-        script.async = true;
-        script.src = 'bower_components/webcomponentsjs/webcomponents-lite.min.js';
-        document.head.appendChild(script);
-    }
 
     // load pre-caching service worker
     if ('serviceWorker' in navigator) {
--- src/Rotalia/FrontendBundle/Resources/source/sw-precache-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    staticFileGlobs: [
-        'images/*/**',
-        'js/*'
-    ]
-};
