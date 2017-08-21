7c8f7f6fc8182940980637d9b5e1a9c4249e09a7
Tanel K <tanel.kiis@gmail.com>
2017-08-21 19:57:25 +0300

    Add tests for credit netting

---
+++ src/Rotalia/APIBundle/Tests/Controller/CreditNettingControllerTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Rotalia\APIBundle\Tests\Controller;
+use Rotalia\APIBundle\Command\CreditNettingCommand;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * Class CreditNettingControllerTest
+ * @package Rotalia\APIBundle\Tests\Controller
+ */
+class CreditNettingControllerTest extends WebTestCase
+{
+    /**
+     * Test list without login
+     */
+    public function testListUnauthorised()
+    {
+        static::$client->request('GET', '/api/creditNettings/');
+        $response = static::$client->getResponse();
+        $result = json_decode($response->getContent());
+
+        $this->assertEquals(403, $response->getStatusCode());
+        $this->assertEquals('Access Denied.', $result->message);
+    }
+
+    /***
+     * Test list with no nettings in the database.
+     */
+    public function testListEmpty()
+    {
+        $this->loginAdmin();
+
+        static::$client->request('GET', '/api/creditNettings/');
+        $response = static::$client->getResponse();
+        $result = json_decode($response->getContent());
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEmpty($result->data->creditNettings);
+
+    }
+
+    /***
+     * test list after netting is done
+     */
+    public function testList()
+    {
+
+        // run credit netting
+        $kernel = $this->createKernel();
+        $kernel->boot();
+
+        $application = new Application($kernel);
+        $application->add(new CreditNettingCommand());
+
+        $command = $application->find('app:credit-netting');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array('command' => $command->getName()));
+
+        // test controller
+        $this->loginAdmin();
+
+        static::$client->request('GET', '/api/creditNettings/');
+        $response = static::$client->getResponse();
+        $result = json_decode($response->getContent());
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertCount(1, $result->data->creditNettings);
+
+    }
+
+}
