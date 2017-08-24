a676184dfb24dd288133c29e627d6dae49035927
Jaak Tamre <jaak.tamre@gmail.com>
2017-08-24 09:52:55 +0300

    #92 Add isActive filter for members request

--- src/Rotalia/APIBundle/Tests/Controller/MembersControllerTest.php
+++ src/Rotalia/APIBundle/Tests/Controller/MembersControllerTest.php
@@ -46,4 +46,66 @@ class MembersControllerTest extends WebTestCase
         $memberNames = array_map(function ($a) { return $a->name;}, $result->data->members);
         $this->assertEquals($expectedMembers, $memberNames);
     }
+
+    public function testListIsActiveTrue()
+    {
+        $this->loginSimpleUser();
+
+        static::$client->request('GET', '/api/members/', [
+            'name' => 'keegi',
+            'isActive' => true,
+        ]);
+
+        $response = static::$client->getResponse();
+        $result = json_decode($response->getContent());
+
+        $this->assertEquals(200, $response->getStatusCode(), 'Error: '.$response->getContent());
+        $this->assertTrue(!empty($result->data->members), 'Failed asserting that members were returned');
+        $this->assertInternalType('array', $result->data->members);
+
+        $expectedMembers = ['Keegi Oluline'];
+        $memberNames = array_map(function ($a) { return $a->name;}, $result->data->members);
+        $this->assertEquals($expectedMembers, $memberNames);
+    }
+
+    public function testListIsActiveFalse()
+    {
+        $this->loginSimpleUser();
+
+        static::$client->request('GET', '/api/members/', [
+            'name' => 'keegi',
+            'isActive' => false,
+        ]);
+
+        $response = static::$client->getResponse();
+        $result = json_decode($response->getContent());
+
+        $this->assertEquals(200, $response->getStatusCode(), 'Error: '.$response->getContent());
+        $this->assertTrue(!empty($result->data->members), 'Failed asserting that members were returned');
+        $this->assertInternalType('array', $result->data->members);
+
+        $expectedMembers = ['Mitte Keegi'];
+        $memberNames = array_map(function ($a) { return $a->name;}, $result->data->members);
+        $this->assertEquals($expectedMembers, $memberNames);
+    }
+
+    public function testListIsActiveNull()
+    {
+        $this->loginSimpleUser();
+
+        static::$client->request('GET', '/api/members/', [
+            'name' => 'keegi',
+        ]);
+
+        $response = static::$client->getResponse();
+        $result = json_decode($response->getContent());
+
+        $this->assertEquals(200, $response->getStatusCode(), 'Error: '.$response->getContent());
+        $this->assertTrue(!empty($result->data->members), 'Failed asserting that members were returned');
+        $this->assertInternalType('array', $result->data->members);
+
+        $expectedMembers = ['Keegi Oluline', 'Mitte Keegi'];
+        $memberNames = array_map(function ($a) { return $a->name;}, $result->data->members);
+        $this->assertEquals($expectedMembers, $memberNames);
+    }
 }
