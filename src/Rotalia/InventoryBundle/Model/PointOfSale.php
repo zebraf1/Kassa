1095220925b22b8dafe170b17c47d70b983e4ef1
Tanel K <tanel.kiis@gmail.com>
2017-08-26 15:33:10 +0300

    Visual improvements, mainly on small screens.

--- src/Rotalia/InventoryBundle/Model/PointOfSale.php
+++ src/Rotalia/InventoryBundle/Model/PointOfSale.php
@@ -13,7 +13,7 @@ class PointOfSale extends BasePointOfSale
             'name' => $this->getName(),
             'deviceInfo' => $this->getDeviceInfo(),
             'convent' => $this->getConvent() ? $this->getConvent()->getName() : null,
-            'createdAt' => $this->getCreatedAt()->format('Y-m-d H:i:s'),
+            'createdAt' => $this->getCreatedAt()->format('H:i d.m.Y'),
             'createdBy' => $this->getMember() ? $this->getMember()->getAjaxName() : null,
         ];
     }
