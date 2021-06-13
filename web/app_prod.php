80a01a1959154d9acd00440919c92faff9b1e7d3
Jaak Tamre <jaak.tamre@gmail.com>
2021-06-13 19:32:31 +0300

    Fix invalid composer packages for phing, update select2 to avoid error

--- web/app_prod.php
+++ web/app_prod.php
@@ -1,9 +1,8 @@
 <?php
 
-use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+$loader = require __DIR__.'/../app/bootstrap.php.cache';
 
 // Use APC for autoloading to improve performance.
 // Change 'sf2' to a unique prefix in order to prevent cache key conflicts
